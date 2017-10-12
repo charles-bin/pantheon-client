65bfece91a7146c2f61ed4ed3d96e7510df81830
Charles Bin <charles.bin@gmail.com>
2017-10-12 10:04:26 +0900

    Client only fork

--- server/ranker/serializers.py
+++
@@ -1,9 +0,0 @@
-""" Serializer for games. """
-
-from ranker.models import Game
-from rest_framework import serializers
-
-class GameSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Game
-		fields = ('id', 'title', 'image', 'description', 'developer', 'genre', 'published', 'rank')
--- server/ranker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from ranker import views
-
-urlpatterns = [
-    url(r'^games/$', views.GameList.as_view()),
-    url(r'^games/(?P<pk>[0-9]+)/$', views.GameDetail.as_view()),
-    ]
