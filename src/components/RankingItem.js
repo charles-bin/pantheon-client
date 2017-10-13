6ddb908611b9fffb3ea574c6ca5d410118edfe9b
Charles Bin <charles.bin@gmail.com>
2017-10-13 15:19:46 +0900

    Items now display ranking and upvote, downvote, and vote count

--- src/components/RankingItem.js
+++ src/components/RankingItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ListGroupItem, Image, Row, Col } from 'react-bootstrap'
+import { ListGroupItem, Image, Button, Glyphicon, Row, Col } from 'react-bootstrap'
 
 const centerChildren = {
   display: "flex",
   alignItems: "center",
+}
+
+const voteStyle = {
+  textAlign: "center",
+  fontSize: "18px"
+}
+
+const glyphStyle = {
+  cursor: "pointer",
+  top: "3px",
+  left: "0.4px"
 }
 
 export default function RankingItem(props) {
@@ -22,13 +33,24 @@ export default function RankingItem(props) {
           <Image alt={item.title} src={item.image} responsive />
         </Col>
         <Col
-          xs={9} xsOffset={0}
-          sm={9} smOffset={0}
-          md={9} mdOffset={0}
-          lg={9} lgOffset={0}>
-          <h4>{item.title}</h4>
+          xs={8} xsOffset={0}
+          sm={8} smOffset={0}
+          md={8} mdOffset={0}
+          lg={8} lgOffset={0}>
+          <h4>{item.rank} - {item.title}</h4>
           <p>{item.developer} - {item.genre}</p>
           <p>{item.description}</p>
+        </Col>
+        <Col
+          xs={1} xsOffset={0}
+          sm={1} smOffset={0}
+          md={1} mdOffset={0}
+          lg={1} lgOffset={0}>
+          <div style={voteStyle}>
+            <Glyphicon glyph="chevron-up" style={glyphStyle} />
+            <div>0</div>
+            <Glyphicon glyph="chevron-down" style={glyphStyle} />
+          </div>
         </Col>
       </Row>
     </li>
