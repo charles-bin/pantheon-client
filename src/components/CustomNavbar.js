2c320341ac443091debf63feb01b7ae93de28822
Charles Bin <charles.bin@gmail.com>
2017-10-17 11:01:51 +0900

    Responsive navbar for small width displays

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -16,29 +16,31 @@ export default function CustomNavbar({ id, label, ...props }) {
           <a href="#">Pantheon</a>
         </Navbar.Brand>
       </Navbar.Header>
-      <Nav>
-        <NavDropdown eventKey={1} title="Lists" id="basic-nav-dropdown">
-          <MenuItem eventKey={1.1}>Action</MenuItem>
-          <MenuItem eventKey={1.2}>Another action</MenuItem>
-          <MenuItem eventKey={1.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={1.4}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavDropdown eventKey={2} title="Account" id="basic-nav-dropdown">
-          <MenuItem eventKey={2.1}>Action</MenuItem>
-          <MenuItem eventKey={2.2}>Another action</MenuItem>
-          <MenuItem eventKey={2.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={2.4}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Navbar.Form pullRight>
-        <FormGroup>
-          <FormControl type="text" placeholder="Search" />
-        </FormGroup>
-      </Navbar.Form>
+      <Navbar.Collapse>
+        <Nav>
+          <NavDropdown eventKey={1} title="Lists" id="basic-nav-dropdown">
+            <MenuItem eventKey={1.1}>Action</MenuItem>
+            <MenuItem eventKey={1.2}>Another action</MenuItem>
+            <MenuItem eventKey={1.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={1.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Nav pullRight>
+          <NavDropdown eventKey={2} title="Account" id="basic-nav-dropdown">
+            <MenuItem eventKey={2.1}>Action</MenuItem>
+            <MenuItem eventKey={2.2}>Another action</MenuItem>
+            <MenuItem eventKey={2.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={2.4}>Separated link</MenuItem>
+          </NavDropdown>
+        </Nav>
+        <Navbar.Form pullRight>
+          <FormGroup>
+            <FormControl type="text" placeholder="Search" />
+          </FormGroup>
+        </Navbar.Form>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
